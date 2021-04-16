95dc2397e70c647d1d2224233ce4f834c8b33d7e
dxt99 <82264690+dxt99@users.noreply.github.com>
2021-04-17 01:05:33 +0800

    Add files via upload

---
+++ function_help.py
@@ -0,0 +1,3 @@
+def bantuan(): #belum selesai, perlu data dari login
+    print("==================HELP==================")
+    print()
---
+++ function_save.py
@@ -0,0 +1,38 @@
+#save(datas)->null 
+#melakukan save pada folder yang akan diinput user. Overwrite/new folder function sudah termasuk
+import os
+
+def writecsv(data,folder,file): #membuat csv baru/overwrite pada path yang diberikan
+    f=open(folder+"\\"+file,"w+")
+    for i in range(len(data)):
+        for j in range(len(data[i])):
+            f.write(str(data[i][j]))
+            if j!=len(data[i])-1:
+                f.write(";")
+        f.write("\n")
+    f.close()
+    
+def save(user,gadget,consumable,consumable_history,gadget_borrow_history,gadget_return_history): 
+
+    filename=str(input("Masukkan nama folder penyimpanan:"))
+    print("Saving...")
+    fullfilename=filename
+    #finding the folder
+    for (root, folder, file) in os.walk("C:", topdown=True):
+        if filename in folder:
+            if root!="C:":
+                root+="\\"
+            fullfilename = root+filename
+            fullfilename=fullfilename[2:]
+    #if path doesn't exist, make one
+    if not os.path.exists(fullfilename):
+        os.makedirs(fullfilename)
+    
+    #writing time
+    writecsv(user,fullfilename,"user.csv")
+    writecsv(gadget,fullfilename,"gadget.csv")
+    writecsv(consumable,fullfilename,"consumable.csv")
+    writecsv(consumable_history,fullfilename,"consumable_history.csv")
+    writecsv(gadget_borrow_history,fullfilename,"gadget_borrow_history.csv")
+    writecsv(gadget_return_history,fullfilename,"gadget_return_history.csv")
+    print("Data telah disimpan pada folder "+filename+"!")
