2e40585845d1d581f94f8f5d603bcea4259fdd89
nart4hire <nart4hire@users.noreply.github.com>
2021-05-01 11:11:36 +0700

    f11

--- f11.py
+++ f11.py
@@ -6,25 +6,40 @@ def sort_date(matrix):
     return matrix
 
 
-def find_from_id(reff, item_id):
+def name_from_id(reff, name_id):
+    for entries in reff:
+        if entries[0] == name_id:
+            return entries[2]
+    return 'Name not found!'
+
+
+def item_from_id(reff, item_id):
     for entries in reff:
         if entries[0] == item_id:
             return entries[1]
     return 'Item not found!'
 
 
-def riwayatpinjam(items, data):
+def riwayatpinjam(users, items, data):
     again = True
     matrix = sort_date(data[1:])
     times = 0
     while again is True:
         for i in range(5):
-            item = find_from_id(items, matrix[i + (5 * times)][2])
+            name = name_from_id(users, matrix[i + (5 * times)][1])
+            item = item_from_id(items, matrix[i + (5 * times)][2])
+            if matrix[i + (5 * times)][5] == 'True':
+                returned = 'Sudah Semua'
+            elif matrix[i + (5 * times)][5] == 'False':
+                returned = 'Belum'
+            else:
+                returned = 'Sebagian ({})'.format(matrix[i + (5 * times)][5])
             print('\nID Peminjaman      : {}'.format(matrix[i + (5 * times)][0]))
-            print('Nama Peminjam      : {}'.format(matrix[i + (5 * times)][1]))
+            print('Nama Peminjam      : {}'.format(name))
             print('Nama Gadget        : {}'.format(item))
             print('Tanggal Peminjaman : {}'.format(matrix[i + (5 * times)][3]))
             print('Jumlah             : {}'.format(matrix[i + (5 * times)][4]))
+            print('Sudah dikembalikan : {}'.format(returned))
         while True:
             f_more = input('\nCetak lima lagi? (y/n): ').lower()
             if f_more == 'y' or f_more == 'n':
--- testcsv/generatehistory.py
+++ testcsv/generatehistory.py
@@ -7,16 +7,16 @@ alphabet = list(map(chr, range(97, 123))) + list(map(chr, range(65, 91)))
 def make_csv(divvy):
     if divvy == 1:
         f = open('gadget_borrow_history.csv', 'w')
-        f.write('id,id_peminjam,id_gadget,tanggal_peminjaman,jumlah,is_returned\n')
+        f.write('id;id_peminjam;id_gadget;tanggal_peminjaman;jumlah;is_returned\n')
 
         for i in range(101):
             b = 'U{:03d}'.format(random.randint(1, 101))
             c = 'G{:03d}'.format(random.randint(0, 999))
             d = '{:02d}/{:02d}/{}'.format(random.randint(1, 28), random.randint(1, 12), random.randint(2000, 2020))
-            e = random.randint(1, 100)
+            e = random.randint(10, 100)
             i_r = random.choice([True, False]) if random.randrange(10) > 4 else (random.randrange(e - 2) + 1)
             a = 'T' + b.lstrip('U') + c.lstrip('G') + '{:03d}'.format(e)
-            f.write('{},{},{},{},{},{}\n'.format(a, b, c, d, e, i_r))
+            f.write('{};{};{};{};{};{}\n'.format(a, b, c, d, e, i_r))
 
         f.close()
     elif handle == 2:
