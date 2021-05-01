b605ce91074341e79a962b8dc77a12a9822c5a26
nart4hire <nart4hire@users.noreply.github.com>
2021-05-01 13:10:22 +0700

    f12

--- f11.py
+++ f11.py
@@ -29,17 +29,17 @@ def riwayatpinjam(users, items, data):
             name = name_from_id(users, matrix[i + (5 * times)][1])
             item = item_from_id(items, matrix[i + (5 * times)][2])
             if matrix[i + (5 * times)][5] == 'True':
-                returned = 'Sudah Semua'
+                returned = 'Dikembalikan Semua'
             elif matrix[i + (5 * times)][5] == 'False':
-                returned = 'Belum'
+                returned = 'Belum Dikembalikan'
             else:
-                returned = 'Sebagian ({})'.format(matrix[i + (5 * times)][5])
+                returned = 'Dikembalikan Sebagian ({})'.format(matrix[i + (5 * times)][5])
             print('\nID Peminjaman      : {}'.format(matrix[i + (5 * times)][0]))
             print('Nama Peminjam      : {}'.format(name))
             print('Nama Gadget        : {}'.format(item))
             print('Tanggal Peminjaman : {}'.format(matrix[i + (5 * times)][3]))
             print('Jumlah             : {}'.format(matrix[i + (5 * times)][4]))
-            print('Sudah dikembalikan : {}'.format(returned))
+            print('Status Peminjaman  : {}'.format(returned))
         while True:
             f_more = input('\nCetak lima lagi? (y/n): ').lower()
             if f_more == 'y' or f_more == 'n':
