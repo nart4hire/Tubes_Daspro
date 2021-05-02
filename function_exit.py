70fdc89090a0b66a2b0aa106622f0bce92fe862b
dxt99 <82264690+dxt99@users.noreply.github.com>
2021-05-02 11:46:33 +0800

    Update function_exit.py

--- function_exit.py
+++ function_exit.py
@@ -4,6 +4,6 @@ def keluar(user,gadget,consumable,consumable_history,gadget_borrow_history,gadge
     choice=str(input())
     if choice=="y" or choice=="Y":
         save(user,gadget,consumable,consumable_history,gadget_borrow_history,gadget_return_history)
-    elif choice!="n" or choice=="N":
+    elif choice!="n" or choice!="N":
         print("Masukan tidak valid")
         keluar(user,gadget,consumable,consumable_history,gadget_borrow_history,gadget_return_history)
